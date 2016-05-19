e53dd88a5ac45fe014f515394bc12108d45ac6cb
Younes Megrini <rasetoi@yahoo.fr>
2016-05-19 08:57:39 +0200

    further improvement

--- qbfs.lua
+++ qbfs.lua
@@ -2,7 +2,8 @@ require 'torch'
 require 'nn'
 require 'optim'
 
-local type = "qbf "
+local type = "p cnf "
+print("\nNNQBF\n")
 
 -- Reading file
 file = torch.DiskFile(arg[1], 'r'):readString("*a")
@@ -24,33 +25,47 @@ variables = tonumber(list())
 clauses = tonumber(list())
 
 formula = torch.Tensor(clauses, variables + 1):zero()
+quantifier = torch.Tensor(variables):zero()
+
+u = 0
+e = 0
+
+-- Parsing quantifiers (placeholder)
+for i=1,variables do
+   if i % 2 == 0
+   then
+      quantifier[i] = 1
+      e = e + 1
+   end
+end
+u = variables - e
 
 -- Parsing clauses
 local cl = 1
-local v = 0
+local var = 0
 
 while cl <= clauses do
 
-   v = tonumber(list())
+   var = tonumber(list())
 
-   if v < -variables or v > variables
+   if var < -variables or var > variables
    then
-      print(string.format("Incoherent values. %d [%d]", v, variables))
+      print(string.format("Incoherent values. %d [%d]", var, variables))
       return
-   elseif v == 0
+   elseif var == 0
    then
       cl = cl + 1
-   elseif v > 0
+   elseif var > 0
    then
-      formula[cl][v] = 1
+      formula[cl][var] = 1
       formula[cl][variables + 1] = formula[cl][variables + 1] + 1
    else
-      formula[cl][-v] = -1
+      formula[cl][-var] = -1
       formula[cl][variables + 1] = formula[cl][variables + 1] + 1
    end
 end
 print("Formula:")
-print(formula)
+print(quantifier, formula)
 
 -- Creating a neural network
 function init(size)
@@ -58,9 +73,9 @@ function init(size)
    local hidden = size
    local output = 1
    net:add(nn.Linear(variables, hidden))
-   net:add(nn.Tanh())
+   net:add(nn.Sigmoid())
    net:add(nn.Linear(hidden, output))
-   net:add(nn.Tanh())
+   net:add(nn.Sigmoid())
    return net
 end
 
@@ -72,7 +87,9 @@ print(uni)
 -- print(uni(torch.Tensor(variables):fill(0)))
 -- print(exi(torch.Tensor(variables):fill(0)))
 
+
 -- Making random choice
+--[[ 
 function play(output)
    local v = 2 * torch.uniform() -1 + output
    if v < 0
@@ -82,32 +99,48 @@ function play(output)
       return 1
    end
 end
+]]
 print("")
 
 -- showing progression
 score = 0
 count = 0
 function percentage(i)
-   if i == 0 then
+   if i == -1 then
       --print(string.format("%d%%", math.floor(100*score/count)))
       io.write(string.format("\r\r\r\r\r%d%% ", math.floor(100*score/count)))
    else
-      score = score + (i+1)/2
+      score = score + i
       count = count + 1
    end
 end
 
+-- Making a play
+function play(model, s, i)
+   s[1][i] = -1
+   local vf = model(s[1])[1]
+   s[1][i] = 1
+   local vt = model(s[1])[1]
+   if vf < vt
+   then
+      s[1][i] = 1
+      s[2][i] = vt
+   else
+      s[1][i] = -1
+      s[2][i] = vf
+   end
+end
+   
 -- Running an evaluation session
 function session()
    local s = torch.Tensor(2, variables):fill(0)
    for i = 1,variables do
-      if i % 2 == 0
+      if quantifier[i] == 0
       then
-	 s[2][i] = exi(s[1])
+	 play(uni, s, i)
       else
-	 s[2][i] = uni(s[1])
+	 play(exi, s, i)
       end
-      s[1][i] = play(s[2][i])
    end
    return s
 end
@@ -135,7 +168,7 @@ function result(s)
 	       f[cl][variables + 1] = f[cl][variables + 1] - 1
 	       if f[cl][variables + 1] == 0
 	       then
-		  percentage(-1)
+		  percentage(0)
 		  return s, v, -1
 	       end
 	    end
@@ -147,37 +180,36 @@ end
 
 -- Building training data sets
 function build(s, v, r)
-   print(s, v, r)
+   
+   --print(s, v, r)
+   
    local lambda = .5
-   local u = math.floor((variables + 1)/2)
-   local e = math.floor(variables/2)
    local uni_set = torch.Tensor(u, variables):zero()
    local exi_set = torch.Tensor(e, variables):zero()
    local uni_val = torch.Tensor(u)
    local exi_val = torch.Tensor(e)
+   local uni_size = 0
+   local exi_size = 0
 
-   local function pull(value, target, factor, i)
-      print(value, target, factor, i)
-      if (i>v) then return value
-      else return value * (1-factor) + target * factor
-      end
+   local function shift(value, target, factor)
+      return factor * value + (1 - factor) * (target + 1) / 2
    end
 
    for i=1,v do
       local f = lambda ^ math.max(v-i,0)
-      if i % 2 == 0
+      if quantifier[i] == 1
       then
 	 for j=i/2,e-1 do
 	    exi_set[j+1][i] = s[1][i]
 	    if j<u then uni_set[j+1][i] = s[1][i] end
 	 end
-	 exi_val[i/2] = pull(s[2][i], s[1][i], f, i) -- incentivizing
+	 exi_val[i/2] = shift(s[2][i], -r * s[1][i], f) -- learning
       else
 	 for j=(i-1)/2+1,u do
 	    if j<u then uni_set[j+1][i] = s[1][i] end
 	    exi_set[j][i] = s[1][i]
 	 end
-	 uni_val[(i-1)/2+1] = pull(s[2][i], -s[1][i], f, i) -- disincentivizing
+	 uni_val[(i-1)/2+1] = shift(s[2][i], r * s[1][i], f) -- learning
       end
    end
    return uni_set, uni_val, exi_set, exi_val
@@ -209,13 +241,13 @@ function train(model, input, target)
    end
 
    sgd_params = {
-      learningRate = .1,
+      learningRate = .01,
       learningRateDecay = .0001,
       weightDecay = 0,
       momentum = 0
    }
 
-   for i = 1,100*variables do
+   for i = 1,1e3 do
       for i =1,(#target)[1] do
 	 _,fs = optim.sgd(eval, x, sgd_params)
       end
@@ -227,10 +259,18 @@ print("Running:")
 -- running the algorithm
 n = 1
 while n>0 do
-   us, uv, es, ev = build(result(session()))
+   local s, r, v = result(session())
+   local us, uv, es, ev = build(result(session()))
    train(uni, us, uv)
    train(exi, es, ev)
-   percentage(0)
+   if n % 1 == 0
+   then
+      print("Session " .. n ..":")
+      print(s, r, v)
+      print("Training set: ")
+      print(us, uv, es, ev)
+      percentage(-1)
+      print("")
+   end
    n = n-1
 end
-print("")
