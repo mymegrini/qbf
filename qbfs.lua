b7b22e77d7b6b3e6704e956edd0da93cf4491763
Younes Megrini <rasetoi@yahoo.fr>
2016-06-05 12:03:52 +0200

    QBFS: added support for Qdimacs format, improved output

--- qbfs.lua
+++ qbfs.lua
@@ -19,33 +19,128 @@ end
 
 -- Parsing variable and clause counts
 index = type:len() + 1
-list = file:gmatch("[-]?%d+")
+list = file:gmatch("[-]?%d+") --looking for integers
 
 variables = tonumber(list())
 clauses = tonumber(list())
 
 formula = torch.Tensor(clauses, variables + 1):zero()
-quantifier = torch.Tensor(variables):zero()
+index = torch.Tensor(1, variables):zero()
+quantifier = torch.Tensor(1, variables):zero()
 
-u = 0
-e = 0
+-- Parsing atom sets
+function quantifiers(s)
 
--- Parsing quantifiers (placeholder)
-for i=1,variables do
-   if i % 2 == 0
-   then
-      quantifier[i] = 1
-      e = e + 1
+   local quantindex = torch.Tensor(variables):fill(1) -- default quantifier is e
+   local atomindex = torch.Tensor(variables):zero()
+   -- Indexing atom sets
+   local function atoms(s, i, q)
+
+      list = s:gmatch("[-]?%d+")
+      var = tonumber(list())
+      while (var ~= 0) do
+	 if var < 0 or var > variables
+	 then
+	    print(string.format("Incorrect atom value. %d [%d]",
+				var, variables))
+	    return
+	 else
+	    atomindex[var] = i
+	    quantindex[var] = q
+	 end
+	 var = tonumber(list())
+      end
+      return iter
+   end
+
+   -- Finding atom sets and indexing them
+   local function sets(s, i)
+
+      local a = s:find("a ")
+      local e = s:find("e ")
+      --[[
+      print("sets:")
+      print(s)
+      print(a, e)
+      ]]
+      if a ~= nil
+      then
+	 if e ~= nil
+	 then
+	    if a < e
+	    then
+	       iter = atoms(s:sub(a+2), i, 0)
+	       return a + 2
+	    else
+	       iter = atoms(s:sub(e+2), i, 1)
+	       return e + 2
+	    end
+	 else
+	    iter = atoms(s:sub(a+2), i, 0)
+	    return a+2
+	 end
+      else
+	 if e ~= nil
+	 then
+	    iter = atoms(s:sub(e+2), i, 1)
+	    return e + 2
+	 else
+	    return nil
+	 end
+      end
+   end
+
+   local i = 1
+   local set = 1
+   local s = file
+
+   -- Parsing sets
+   while set ~= nil do
+      s = s:sub(set)
+      set = sets(s, i)
+      i = i + 1
+   end
+
+   --[[
+   print("Reindexing:")
+   print(atomindex)
+   print(quantindex)
+   ]]
+
+   local qset = 0
+   local var = 1
+   while (var <= variables) do
+      for i=1,variables do
+	 if atomindex[i] == qset
+	 then
+	    index[1][i] = var
+	    quantifier[1][var] = quantindex[i]
+	    var = var + 1
+	 end
+      end
+      qset = qset + 1  -- Switching to next set
    end
 end
-u = variables - e
+
+print("Reindexing:")
+quantifiers(file)
+print(index)
+print("Quantifiers:")
+-- Counting atoms for each quantifier
+u = 0
+e = 0
+for i=1,variables do
+   e = e + quantifier[1][i]
+   u = u + (1-quantifier[1][i])
+end
+print(quantifier)
 
 -- Parsing clauses
 local cl = 1
 local var = 0
 
 while cl <= clauses do
-
+   --print(cl)
    var = tonumber(list())
 
    if var < -variables or var > variables
@@ -57,15 +152,13 @@ while cl <= clauses do
       cl = cl + 1
    elseif var > 0
    then
-      formula[cl][var] = 1
+      formula[cl][index[1][var]] = 1
       formula[cl][variables + 1] = formula[cl][variables + 1] + 1
    else
-      formula[cl][-var] = -1
+      formula[cl][index[1][-var]] = -1
       formula[cl][variables + 1] = formula[cl][variables + 1] + 1
    end
 end
-print("Quantifier:")
-print(quantifier)
 print("Formula:")
 print(formula)
 
@@ -110,7 +203,6 @@ count = 0
 function percentage(i)
    if i == -1 then
       print(string.format("%d%%", math.floor(100*score/count)))
-      --io.write(string.format("\r\r\r\r\r%d%% ", math.floor(100*score/count)))
    else
       score = score + i
       count = count + 1
@@ -151,7 +243,7 @@ end
 function session()
    local s = torch.Tensor(2, variables):fill(0)
    for i = 1,variables do
-      if quantifier[i] == 0
+      if quantifier[1][i] == 0
       then
 	 play(uni, s, i)
       else
@@ -198,7 +290,6 @@ end
 function build(s, v, r)
 
    --print(s, v, r)
-
    local lambda = .5
    -- learning inputs
    local uni_set = torch.Tensor(u, variables):zero()
@@ -218,7 +309,7 @@ function build(s, v, r)
 
    for i=1,v do
       local f = lambda ^ math.max(v-i,0)
-      if quantifier[i] == 1
+      if quantifier[1][i] == 1
       then
 	 e_i = e_i + 1
 	 for k=1,i do exi_set[e_i][k] = s[1][k] end
@@ -261,46 +352,78 @@ function train(model, input, target, size)
 
       sgd_params = {
 	 learningRate = .01,
-	 learningRateDecay = .0001,
+	 learningRateDecay = .001,
 	 weightDecay = 0,
 	 momentum = 0
       }
 
-      for i = 1,1e3 do
+      local l = 0
+      for i = 1,1e2 do
+	 current_loss = 0
 	 for i =1,(#target)[1] do
 	    _,fs = optim.sgd(eval, x, sgd_params)
+	    current_loss = current_loss + fs[1]
 	 end
+	 current_loss = current_loss / (#target)[1]
+	 l = l + current_loss
       end
+      return l/1e3
    end
 end
 
-print("Running:")
+print("Press Enter to start.")
+io.stdin:read('*l')
+print("Running:\n")
+print("-------------------------------------------------------------------------------\n")
 
 -- running the algorithm
-n = 1000 -- total number of sessions
-game = 1 -- current number of game sessions
+game = 0 -- current number of game sessions
+-- Choosing number of epochs
+if #arg >= 2
+then
+   n = tonumber(arg[2])
+else
+   n = 1000
+end
+if #arg >= 3
+then
+   step = tonumber(argv[3])
+else
+   step = 1
+end
+t = os.clock()
 while n>0 do
    local s, v, r = result(session())
    local us, uv, ui, es, ev, ei = build(result(session()))
-   train(uni, us, uv, ui)
-   train(exi, es, ev, ei)
-   if n % 10 == 0
+   local ul = train(uni, us, uv, ui)
+   local el = train(exi, es, ev, ei)
+   if os.clock() > t + step
    then
+      t = os.clock()
       print("Session " .. game ..":")
       print(s)
-      print("Result: " .. r .. " (var " .. v .. ")")
-
-      print("Training set: ")
-      print("uni:")
-      print(us, uv, ui)
-
-      print("exi:")
-      print(es, ev, ei)
-
+      print("Result: " .. ((r+1)/2) .. " (var " .. v .. ")\n")
+      --[[
+      print("Training set:\n")
+      print("uni: " .. ul)
+      print(us, uv)
+      print("exi: " .. el)
+      print(es, ev)
+      ]]
       print("\nPercentage:")
       percentage(-1)
-      print("------------------------------------------------------\n")
+print("-------------------------------------------------------------------------------\n")
    end
    game = game + 1
    n = n-1
 end
+
+-- print final session without exploration
+explore = false
+local s, v, r = result(session())
+print("Session " .. game ..":")
+print(s)
+print("Result: " .. ((r+1)/2) .. " (var " .. v .. ")")
+print("\nPercentage:")
+percentage((r+1)/2)
+percentage(-1)
